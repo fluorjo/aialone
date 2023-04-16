d472f8441adc3afd31b289166718a02681478366
fluorjo <fluorjo@naver.com>
2023-04-16 13:06:47 +0900

    getmodules 작성

---
+++ code/3-practice/misc/getCIFARMeanStd.py
@@ -0,0 +1,3 @@
+from torchvision.datasets import CIFAR10
+
+train_dataset=CIFAR10(root='./)
---
+++ code/3-practice/utils/getModules.py
@@ -0,0 +1,38 @@
+#3.데이터 로더. getDataLoader
+
+#평균, 표준편차
+from .tools import CIFAR_MEAN,CIFAR_STD
+#transform
+    #크기 변경
+from torchvision.transforms import Resize
+    #텐서로
+from torchvision.transforms import ToTensor
+    #정규화
+from torchvision.transforms import Normalize 
+    #위의 것들을 묶어서 할 수 있게 해주는 기능
+from torchvision.transforms import Compose
+
+#데이터셋
+from torchvision.datasets import CIFAR10
+#데이터 로더
+from torch.utils.data import DataLoader
+
+def getDataLoader(args):
+    #평균, 표준편차
+    mean=CIFAR_MEAN
+    std=CIFAR_STD
+    
+    #transform
+    transform=Compose([
+        Resize((args.img_size,args.img_size)),
+        ToTensor(),
+        Normalize(mean,std)
+    ])
+    #데이터셋 삽입
+    train_dataset=CIFAR10(root='./cifar',train=True, transform=transform, download=True)
+    test_dataset=CIFAR10(root='./cifar',train=False, transform=transform, download=True)
+    #데이터로더 삽입
+    train_loader=DataLoader(dataset=train_dataset,batch_size=args.batch_size,shuffle=True)
+    test_loader=DataLoader(dataset=test_dataset,batch_size=args.batch_size,shuffle=False)
+    
+    return train_loader, test_loader
