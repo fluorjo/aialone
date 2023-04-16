acbc70cc12290860c825224f87bd498af99238c8
fluorjo <fluorjo@naver.com>
2023-04-16 23:36:55 +0900

    2주차 ipynb 확장자로 수정

--- code/3-practice/train.py
+++ code/3-practice/train.py
@@ -1,11 +1,10 @@
 #1 parser
 from utils.parser import parser_args
-#2 평균, 분산
-from utils.tools import CIFAR_MEAN, CIFAR_STD
+
 #3 데이터로더
 from utils.getModules import getDataLoader
 def main():
     #1 parser_args
     args=parser_args()
-    #
+    #3 데이터로더
     train_loader, test_loader = getDataLoader(args)
--- code/3-practice/utils/tools.py
+++ code/3-practice/utils/tools.py
@@ -1,5 +1,9 @@
 import torch
 import cv2
-
 CIFAR_MEAN=[0.49139968, 0.48215827, 0.44653124]
 CIFAR_STD=[0.24703233, 0.24348505, 0.26158768]
+
+def draw_from_dataset(dataset,std,mean):
+    def reverse_trans(x):
+        x=(x*std)+mean
+        return x.clamp(0,1)*255
