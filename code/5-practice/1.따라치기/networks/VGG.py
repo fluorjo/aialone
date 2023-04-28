459046cc4f58ed28f6b8ff476bed5dae296b0041
fluorjo <fluorjo@naver.com>
2023-04-28 21:13:28 +0900

    5주차 코드 익히기

---
+++ code/5-practice/1.따라치기/networks/VGG.py
@@ -0,0 +1,62 @@
+import torch.nn as nn
+
+class VGG_conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        super().__init__()
+        padding=1 if kernel_size==3 else 0
+        
+        self.conv=nn.Conv2d(
+            in_channels=in_channels, 
+            out_channels=out_channels, 
+            kernel_size=kernel_size, 
+            stride=1,padding=padding)
+        
+        self.bn=nn.BatchNorm2d(out_channels)
+        self.relu=nn.ReLU()
+        
+    def forward(self,x):
+        x=self.conv(x)
+        x=self.bn(x)
+        x=self.relu(x)
+        return x
+
+class VGG_Block(nn.Module):
+    def __init__(self,in_channel, output_channel,num_convs, last_1conv=False):
+        super().__init__()
+        self.first_conv=VGG_conv(in_channel,output_channel)
+        self.middle_convs=nn.ModuleList([
+            VGG_conv(output_channel, output_channel) for _ in range(num_convs-2)
+        ])
+        kernel_size=1 if last_1conv else 3
+        self.last_convs=VGG_conv(output_channel, output_channel, kernel_size=kernel_size)
+        
+        self.mp=nn.MaxPool2d(kernel_size=2,stride=2)
+    
+    def forward(self,x):
+        x=self.first_conv(x)
+        for module in self.middle_convs:
+            x=module(x)
+        x=self.mp(x)
+        return x
+    
+class VGG_classifier(nn.Module):
+    def __init__(self,num_classes):
+        super().__init__()
+        self.fc=nn.Sequential(
+            nn.Linear(25088,4096),
+            nn.Linear(4096,4096),
+            nn.Linear(4096,num_classes),
+        ),
+        
+    def forward(self,x):
+        x=self.fc(x)
+        return x
+    
+class VGG_A(nn.Module):
+    def __init__(self, num_classes):
+        super().__ini__()
+        self.VGG_Block1=VGG_Block(3,64,1)
+        self.VGG_Block2=VGG_Block(64,128,1)
+        self.VGG_Block3=VGG_Block(128,256,2)
+        self.VGG_Block4=VGG_Block(256,512,2)
+        self.VGG_Block5=VGG_Block(512,512,2)
